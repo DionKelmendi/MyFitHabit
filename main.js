97c22288e875d2e02faea3f5894507ae4a24403f
DionKelmendi <108992835+DionKelmendi@users.noreply.github.com>
2023-01-08 16:47:29 +0100

    added/changed some text

--- main.js
+++ main.js
@@ -1,4 +1,62 @@
 
+ var x = 0;
+
+  window.onscroll = function () { scrollFunction() };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.querySelector("header").style.opacity = 0;
+    } else {
+      document.querySelector("header").style.opacity = 70 + "%";
+    }
+  }
+
+  function sliderLeft() {
+
+    var slider = document.querySelector("#slider");
+    var sliderCount = document.querySelector("#slider").children.length;
+    var sliderItems = document.querySelectorAll("#sliderItem")
+
+    if (x < 0) {
+
+      x += 350;
+    }
+
+    sliderItems.forEach(si => {
+
+      si.style.transform = "translateX(" + x + "px)";
+
+    });
+
+    // console.log("Going Left")
+    // console.log(x)
+    // console.log(sliderCount)
+  }
+
+  function sliderRight() {
+
+    var slider = document.querySelector("#slider");
+    var sliderCount = document.querySelector("#slider").children.length;
+    var sliderItems = document.querySelectorAll("#sliderItem")
+
+    // console.log(sliderItems);
+
+    if (x > ((sliderCount - 3) * -350)) {
+
+      x -= 350;
+    }
+
+    sliderItems.forEach(si => {
+
+      si.style.transform = "translateX(" + x + "px)";
+
+    });
+
+    // console.log("Going Right")
+    // console.log(x)
+    // console.log(sliderCount)
+  }
+
 (function ($) {
     "use strict";
 
